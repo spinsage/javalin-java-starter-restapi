391ab9aa23686c4b47a12a3d2f6b00fa7c7f5e52
spinsager <63341255+spinsager@users.noreply.github.com>
2022-01-15 04:55:03 +0530

    Fix/docker support (#3)
    * Added docker suppport
    
    * Update README.md

--- src/main/java/com/spinsage/starter/javalinrest/RestApi.java
+++ src/main/java/com/spinsage/starter/javalinrest/RestApi.java
@@ -7,7 +7,8 @@ public class RestApi {
     public static void main(String[] args) {
         ProductController productController = new ProductController();
 
-        Javalin app = Javalin.create().start(7000);
+        int port = System.getenv("SERVER_PORT") != null? Integer.parseInt(System.getenv("SERVER_PORT")) : 8080;
+        Javalin app = Javalin.create().start(port);
         app.get("/product", productController.getAllProducts);
         app.get("/product/:id", productController.getById);
         app.post("/product", productController.addProduct);
